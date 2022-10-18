78f55b3463058016e13c63666d747e5665ddcec5
Admin <admin@Admins-MacBook-Pro.local>
2022-10-18 01:49:42 +0100

    custom url scheme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const android_url = 'https://play.google.com/store/apps/details?id=com.seamless_hrms';
+const ios_url = 'https://apps.apple.com/us/app/seamlesshrms/id1549679995'
 function App() {
+  const { mobile, platform } = navigator.userAgentData
+  if (mobile) {
+    setTimeout(function () {
+      if (platform.toLowerCase() === 'android') window.location = android_url;
+      if (platform.toLowerCase() === 'ios') window.location = ios_url;
+    }, 100);
+    window.location.href = "breezeapp://";
+  }
   return (
     <div className="App">
       <header className="App-header">
